8355365e5391d0cd239081c97b5c9c15e2271202
Abdullah Muchsin <Abdullahmuchsin96@gmail.com>
2025-07-19 01:34:49 +0700

    Update Kode

---
+++ assets/js/tailwinds.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        foxOrange: "#e86a12",
+        foxBrown: "#9d4b00",
+        foxLight: "#f9e1c7",
+        foxDark: "#2b1700",
+      },
+    },
+  },
+};
